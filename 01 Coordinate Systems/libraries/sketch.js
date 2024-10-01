cb290127bb890edd3e431d365c11861c1d24fd7d
Mishal21t <mishaltauseef6@gmail.com>
2024-10-01 11:12:33 -0600

    refactoring

---
+++ 01 Coordinate Systems/libraries/sketch.js
@@ -0,0 +1,14 @@
+// Events (Mouse/ keyboard)
+// Mishal
+// 12 Sep 2024
+//
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  print(width,height);
+}
---
+++ 01 Coordinate Systems/sketch.js
@@ -0,0 +1,23 @@
+// Coordinate Systems
+// Mishal
+// 11 sep 2024
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  print("Setup runs once, at the start")
+  createCanvas(500, 400);
+  //         width  height
+}
+
+function draw() {
+  // runs over and over (targetting 60fps)
+  background(220);
+
+  // draw some circles
+  circle (0,0,50);
+  fill(204,102,0)
+  circle(100,40,50)
+}
