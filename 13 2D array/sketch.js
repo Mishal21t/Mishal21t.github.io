15c64b43df091fca2b7d2266df2332326f497705
Mishal21t <mishaltauseef6@gmail.com>
2024-10-28 12:13:29 -0600

    2D array demo

---
+++ 13 2D array/sketch.js
@@ -0,0 +1,48 @@
+// 2D Array
+// Mishal
+// 28th Oct 2024
+//
+
+let grid = [[0,  255, 200, 50, 100],
+            [255, 50, 150, 250,  0], 
+            [0,    0, 150, 200,  50]];
+
+const NUM_ROWS = 3;
+const NUM_COLS = 5;      
+
+let rectWidth, rectHeight
+
+
+function renderGrid(){
+  for(let x = 0; x < NUM_COLS; x++){
+    for(let y = 0; y < NUM_ROWS; y++){
+      fill(grid[y][x]);
+      rect(x*rectWidth, y*rectHeight, rectWidth, rectHeight);
+      
+
+    }
+  }
+}
+
+function getArrayX(){
+  return int(mouseX/rectWidth);
+  
+}
+
+function getArrayY (){
+  return int(mouseY/rectHeight);
+}
+function setup() {
+  createCanvas(500, 300);
+  rectWidth = 500/NUM_COLS;
+  rectHeight = 300/NUM_ROWS;
+}
+
+function draw() {
+  background(220);
+  renderGrid();
+  if(frameCount%1 === 0){
+    grid[getArrayY()][getArrayX()] = int(random(0, 255));
+  }
+ 
+}
