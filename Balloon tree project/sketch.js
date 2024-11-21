9e88ee9b64304124fd85cd2709d9da6395b8b378
Mishal21t <mishaltauseef6@gmail.com>
2024-11-21 11:00:41 -0600

    balloon tree
    challange feature

--- Balloon tree project/sketch.js
+++ Balloon tree project/sketch.js
@@ -4,7 +4,7 @@
 //
 
 let scale = 15;
-let leafDepth = 18;
+let leafDepth = 6;
 
 function setup() {
   createCanvas(500, 500);
@@ -12,28 +12,38 @@ function setup() {
 }
 
 function draw() {
-  drawTree(width/2, height*0.9, 90, 6);
+  background(255);
+  let angle = map(mouseX, 0, width, 10, 35);
+
+  drawTree(width/2, height*0.9, 90, 6, angle);
   randomSeed(5);
 }
 
 function drawLine( x1, y1, x2, y2, depth) {
+  strokeWeight(map(depth, 1, 6, 1, 8));
 //draw a line segment connecting (x1,y1) to (x2,y2)
   line(x1, y1, x2, y2);
+  strokeWeight(1);
 }
 
-function drawTree(x1, y1, angle, depth) {
+function drawTree(x1, y1, angle, depth, branchAngle) {
   if (depth > 0) {
     let x2 = x1 + (cos(radians(angle))*depth*scale); //calculate endpoints of
     // current branch 
     let y2 = y1 - (sin(radians(angle))*depth*scale); //using trig ratios. Get
     // shorter based on depth
     drawLine(x1, y1, x2, y2, depth);
+    if (depth <= leafDepth){
+      drawLeaf(x2, y2, depth);
+    }
     //for a 2-branch tree:
-    drawTree(x2, y2, angle-18, depth-1);
-    drawTree(x2, y2, angle+18, depth-1);
-    drawTree(x2, y2, angle, depth - 1);
+    drawTree(x2, y2, angle- branchAngle, depth-1, branchAngle);
+    drawTree(x2, y2, angle+ branchAngle, depth-1, branchAngle);
+    drawTree(x2, y2, angle, depth - 1, branchAngle);
 
     drawLeaf(x2, y2, depth);
+  console.log('Depth', depth, 'leafDepth', leafDepth);
+
   }
 }
 
@@ -41,5 +51,15 @@ function drawLeaf(x, y, depth){
   let leafSize = random(5, map(depth, 1, leafDepth, 20, 10));
   fill(random(255), random(255), random(255));
   circle(x, y, leafSize);
+}
 
+function keyPressed(){
+  // decrease leaf depth with'z', but not below 1
+  if ((key === 'z' || key === 'Z') && leafDepth >= 1){
+    leafDepth--;
+  }
+  // increase leaf depthwith 'x' , yp tp maximum depth
+  if ((key === 'x' || key === 'X') && leafDepth <= 1){
+    leafDepth++;
+  }
 }
